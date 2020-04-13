22d7052de2897d43a020b6ab0dc21a0a9f3e08b1
thenatural86 <philip.smalls86@gmail.com>
2020-04-13 11:49:44 -0400

    Updates

---
+++ precache-manifest.53703bf2206939444f19f1749d9fecac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12fc1483530cdc447182f61819dcaae",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "864db70a1226f56229c5",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "1f5a064324844ecbb98a",
+    "url": "/portfolio/static/css/main.93eb21ed.chunk.css"
+  },
+  {
+    "revision": "864db70a1226f56229c5",
+    "url": "/portfolio/static/js/2.4a685e5f.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.4a685e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5a064324844ecbb98a",
+    "url": "/portfolio/static/js/main.21699d7d.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "1bfd9e3e7848056dc8be017001530bbd",
+    "url": "/portfolio/static/media/clean-textile.1bfd9e3e.png"
+  }
+]);
