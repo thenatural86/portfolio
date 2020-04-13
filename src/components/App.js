374a476508b024050adc3d81ef220c435df17eb1
thenatural86 <philip.smalls86@gmail.com>
2020-04-13 15:20:01 -0400

    color

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import "./App.css"
 class App extends React.Component {
   renderTitle = () => {
     return (
-      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+      <Link to="/" style={{ color: "black", textDecoration: "none" }}>
         Philip Smalls{"  "}
         <img
           src="https://i.ya-webdesign.com/images/chucky-lightning-bolt-png-7.png"
@@ -26,18 +26,26 @@ class App extends React.Component {
         <Layout>
           <Header className="header-color" title={this.renderTitle()} scroll>
             <Navigation>
+              <Link style={{ color: "black" }} to="/">
+                Home
+              </Link>
+              <Link style={{ color: "black" }} to="/projects">
+                Projects
+              </Link>
+              <Link style={{ color: "black" }} to="/resume">
+                Resume
+              </Link>
+              <Link style={{ color: "black" }} to="/contact">
+                Contact
+              </Link>
+            </Navigation>
+          </Header>
+          <Drawer title={this.renderTitle()}>
+            <Navigation>
               <Link to="/">Home</Link>
               <Link to="/projects">Projects</Link>
               <Link to="/resume">Resume</Link>
               <Link to="/contact">Contact</Link>
-            </Navigation>
-          </Header>
-          <Drawer title="Title">
-            <Navigation>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
             </Navigation>
           </Drawer>
           <Content>
