3397d29afde976d840935309c7ae0939b3cf3b44
thenatural86 <philip.smalls86@gmail.com>
2020-04-13 13:25:24 -0400

    git pages config

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,6 @@ class Contact extends Component {
         <Grid className="contact-grid">
           <Cell col={12}>
             <h2>Contact Me</h2>
-            {/* <img src={textile} alt="" textile /> */}
             <img
               className="contact-image"
               src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/92849555_10156672183300981_4992975803168849920_n.jpg?_nc_cat=111&_nc_sid=8024bb&_nc_ohc=4i_UsFCwLcgAX-Mi2Lg&_nc_ht=scontent-lga3-1.xx&oh=5d7cc990ac7f6ed7044ceeb5ca61d98e&oe=5EB69FA7"
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
 import LandingPage from "./LandingPage"
-import AboutMe from "./AboutMe"
 import Contact from "./Contact"
 import Projects from "./Projects"
 import Resume from "./Resume"
@@ -10,7 +9,6 @@ const Main = () => {
   return (
     <Switch>
       <Route exact path="/" component={LandingPage} />
-      <Route exact path="/AboutMe" component={AboutMe} />
       <Route exact path="/Projects" component={Projects} />
       <Route exact path="/Contact" component={Contact} />
       <Route exact path="/Resume" component={Resume} />
