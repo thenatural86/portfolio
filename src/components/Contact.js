c14d10b91942a3c37281b679140938a83ff1794d
thenatural86 <philip.smalls86@gmail.com>
2020-04-18 19:52:31 -0400

    background styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,7 +23,7 @@ class Contact extends Component {
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <i class="fab fa-linkedin-in"></i>
+                <i className="fab fa-linkedin-in"></i>
               </a>
 
               <a
@@ -31,35 +31,35 @@ class Contact extends Component {
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <i class="fab fa-github"></i>
+                <i className="fab fa-github"></i>
               </a>
               <a
                 href="https://www.youtube.com/channel/UC16Y3fs7PjLaOdsSGhuW-5g"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <i class="fab fa-youtube"></i>
+                <i className="fab fa-youtube"></i>
               </a>
               <a
                 href="https://medium.com/@philip.smalls86"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <i class="fab fa-medium"></i>
+                <i className="fab fa-medium"></i>
               </a>
               <a
                 href="https://www.instagram.com/ninjajesse/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <i class="fab fa-instagram-square"></i>
+                <i className="fab fa-instagram-square"></i>
               </a>
               <a
                 href="https://www.facebook.com/philip.smalls"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <i class="fab fa-facebook-square"></i>
+                <i className="fab fa-facebook-square"></i>
               </a>
             </div>
           </Cell>
